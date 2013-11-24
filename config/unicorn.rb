dca7bc68e23b08e3ce98a432c1885adb4d5fcf37
Ishay Mamluk <ishay@appforma.com>
2013-11-24 15:17:34 +0200

    added procfile, unicoron config fixes for cloud 66

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,102 +10,40 @@
 
 # Use at least one worker per core if you're on a dedicated server,
 # more will usually help for _short_ waits on databases/caches.
-worker_processes 10
+worker_processes 8
 
-# Since Unicorn is never exposed to outside clients, it does not need to
-# run on the standard HTTP port (80), there is no reason to start Unicorn
-# as root unless it's from system init scripts.
-# If running the master process as root and the workers as an unprivileged
-# user, do this to switch euid/egid in the workers (also chowns logs):
-# user "unprivileged_user", "unprivileged_group"
+working_directory "#{ENV['RAILS_STACK_PATH']}"
 
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
-#working_directory "#{Rails.root.to_s}/current" # available in 0.94.0+
+listen "/tmp/web_server.sock", :backlog => 64
 
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen "/tmp/.sock", :backlog => 64
-listen ENV['PORT'], :tcp_nopush => true
+timeout 120
 
-# nuke workers after 30 seconds instead of 60 seconds (the default)
-timeout 30
+pid '/tmp/web_server.pid'
 
-# feel free to point this anywhere accessible on the filesystem
-#pid "/app/shared/pids/unicorn.pid"
+stderr_path "#{ENV['RAILS_STACK_PATH']}/log/unicorn.stderr.log"
+stdout_path "#{ENV['RAILS_STACK_PATH']}/log/unicorn.stdout.log"
 
-# By default, the Unicorn logger will write to stderr.
-# Additionally, ome applications/frameworks log to stderr or stdout,
-# so prevent them from going to /dev/null when daemonized here:
-#stderr_path "#{Rails.root}/log/unicorn.stderr.log"
-#stdout_path "/log/unicorn.stdout.log"
-
-# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
-# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
-preload_app true # ***Set to true when working with NewRelic
+preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
   GC.copy_on_write_friendly = true
 
+check_client_connection false
+
 before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
+  old_pid = '/tmp/web_server.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end 
+
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
-  end
-
-  # If you are using Redis but not Resque, change this
-  # if defined?(Resque)
-  #   Resque.redis.quit
-  #   Rails.logger.info('Disconnected from Redis')
-  # end
-
-  # The following is only recommended for memory/DB-constrained
-  # installations.  It is not needed if your system can house
-  # twice as many worker_processes as you have configured.
-  #
-  # # This allows a new master process to incrementally
-  # # phase out the old master process with SIGTTOU to avoid a
-  # # thundering herd (especially in the "preload_app false" case)
-  # # when doing a transparent upgrade.  The last worker spawned
-  # # will then kill off the old master process with a SIGQUIT.
-  # old_pid = "#{server.config[:pid]}.oldbin"
-  # if old_pid != server.pid
-  #   begin
-  #     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-  #     Process.kill(sig, File.read(old_pid).to_i)
-  #   rescue Errno::ENOENT, Errno::ESRCH
-  #   end
-  # end
-  #
-  # Throttle the master from forking too quickly by sleeping.  Due
-  # to the implementation of standard Unix signal handlers, this
-  # helps (but does not completely) prevent identical, repeated signals
-  # from being lost when the receiving process is busy.
-  
-  sleep 1
 end
 
 after_fork do |server, worker|
-  # per-process listener ports for debugging/admin/migrations
-  # addr = "127.0.0.1:#{9293 + worker.nr}"
-  # server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
-
-  # the following is *required* for Rails + "preload_app true",
-  if defined?(ActiveRecord::Base)
+  defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
-  end
-
-  # If you are using Redis but not Resque, change this
-  # if defined?(Resque)
-  #   Resque.redis = ENV['REDIS_URI']
-  #   Rails.logger.info('Connected to Redis')
-  # end
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
 end
