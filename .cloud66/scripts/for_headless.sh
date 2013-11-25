cefe4e687cc5514cbceae40dc85b4d2a328cda9b
Ishay Mamluk <ishay@appforma.com>
2013-11-25 02:36:31 +0200

    trying to reuse headless

--- .cloud66/scripts/for_headless.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /var/.cloud66_env
-sudo Xvfb :99 -ac
-export DISPLAY=:99
-
--- app/middlewares/image_processing/renderers/html.rb
+++ app/middlewares/image_processing/renderers/html.rb
@@ -16,36 +16,35 @@ module ImageProcessing
         puts "---- START: #{start_time} ----"
         puts ">>>>>>>>>>>>>>>>>>> #TMP PTH: #{temp_path}"
 
-        Headless.ly do
-          driver = Selenium::WebDriver.for :firefox
+        Headless.new(display: 100, reuse: true).start
+        driver = Selenium::WebDriver.for :firefox
 
-          begin
-            url = CGI.parse(url).keys.first
-            element = driver.get url
-            sleep 0.5.seconds # Wait for javascript load to complete
-            element_id = options[:element_id]
+        begin
+          url = CGI.parse(url).keys.first
+          element = driver.get url
+          sleep 0.5.seconds # Wait for javascript load to complete
+          element_id = options[:element_id]
 
-            unless element_id.blank?
-              requested_element = driver.find_element(:id => element_id)
+          unless element_id.blank?
+            requested_element = driver.find_element(:id => element_id)
 
-              driver.manage.window.resize_to(requested_element.size.width, requested_element.size.height)
-              requested_element.location_once_scrolled_into_view
+            driver.manage.window.resize_to(requested_element.size.width, requested_element.size.height)
+            requested_element.location_once_scrolled_into_view
 
-              driver.save_screenshot(temp_path)
-              sleep 0.5.seconds # Make sure screenshot is properly saved 
+            driver.save_screenshot(temp_path)
+            sleep 0.5.seconds # Make sure screenshot is properly saved 
 
-              # For some reason, the firefox webdriver's screen shot size is higher than the actual window size - Crop it to its real size
-              cmd = "convert #{temp_path} -crop #{requested_element.size.width}x#{requested_element.size.height}+0+20 +repage #{temp_path}"  
-              puts ">>>>>>>>>>>>>> Crop: \n#{cmd}" 
-              system(cmd)
-            else
-              driver.manage.window.maximize
-              driver.save_screenshot(temp_path)
-            end
-          ensure
-            driver.quit
-          end  
-        end 
+            # For some reason, the firefox webdriver's screen shot size is higher than the actual window size - Crop it to its real size
+            cmd = "convert #{temp_path} -crop #{requested_element.size.width}x#{requested_element.size.height}+0+20 +repage #{temp_path}"  
+            puts ">>>>>>>>>>>>>> Crop: \n#{cmd}" 
+            system(cmd)
+          else
+            driver.manage.window.maximize
+            driver.save_screenshot(temp_path)
+          end
+        ensure
+          driver.quit
+        end  
 
         end_time = Time.now
         puts "---- END: #{end_time} ----"
